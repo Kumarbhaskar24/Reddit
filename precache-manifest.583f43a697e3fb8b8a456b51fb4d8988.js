56618a74d3ec25950606498fa3c68ada557c2880
Kumar <krbhaskar4321@gmail.com>
2022-05-06 11:39:38 +0530

    Updates

---
+++ precache-manifest.583f43a697e3fb8b8a456b51fb4d8988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed2256149fa7f42cafec",
+    "url": "/Reddit/static/css/main.4ed975a0.chunk.css"
+  },
+  {
+    "revision": "ed2256149fa7f42cafec",
+    "url": "/Reddit/static/js/main.ed225614.chunk.js"
+  },
+  {
+    "revision": "5bc3629560d66c69bfc6",
+    "url": "/Reddit/static/js/1.5bc36295.chunk.js"
+  },
+  {
+    "revision": "e7ec84fa70d8233089d5",
+    "url": "/Reddit/static/js/runtime~main.e7ec84fa.js"
+  },
+  {
+    "revision": "fd5a6f3b552bff01fada0ffbac7b1d4d",
+    "url": "/Reddit/static/media/logo.fd5a6f3b.png"
+  },
+  {
+    "revision": "e43d7354f2d3f4778fb302c3bc46629f",
+    "url": "/Reddit/index.html"
+  }
+];
